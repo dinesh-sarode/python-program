498074260936bc2a3424c3671c6792ec63a8c0d6
Dinesh Sarode <110563726+dinesh-sarode@users.noreply.github.com>
2022-11-21 18:35:05 +0530

    Add files via upload

---
+++ Python_program.py
@@ -0,0 +1,14 @@
+
+#Defining a Function to print Squared Number
+def print_square_value(numbers):
+    for number in numbers:
+        if number != 2:
+            squared = number * number
+            print(squared)
+
+numbers = []
+for x in range():
+    n = input('Enter your List: \n')
+    numbers.append(n)
+
+print_square_value(numbers)
